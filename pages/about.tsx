a147acecbe71d3876e84947d337dd8ceb8197bab
Reese Sapphire <reese@ovine.xyz>
2022-03-19 14:41:50 -0500

    fixed about page (again)

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 import Page from '@/components/page'
 import {
 	P, A
@@ -11,23 +9,23 @@ import {
 	mdiGithub
 } from '@mdi/js'
 
-import logo from '@/logo/logo-circular.svg'
-
 const About = () => (
-	<Page className='justify-center items-center gap-10'>
-		<Image
-			src={logo}
-			alt='circular logo'
-			width={128}
-			height={128} />
+	<Page className='h-full'>
+		<div className='h-full flex flex-col justify-center items-center gap-10'>
+			<img
+				src='/images/logo-circular.svg'
+				alt='WebToys logo'
+				width={128}
+				height={128} />
 
-		<P className='text-center'>
-			WebToys is made with <Icon path={mdiHeart} aria-label='love' size={1} className='inline-block text-pink-500' /> by <A href='https://github.com/reeseovine'>Reese Sapphire</A>
-		</P>
+			<P className='text-center'>
+				WebToys is made with <Icon path={mdiHeart} aria-label='love' size={1} className='inline-block text-pink-500' /> by <A href='https://github.com/reeseovine'>Reese Sapphire</A>
+			</P>
 
-		<a href='https://github.com/reeseovine/webtoys' className='mb-10'>
-			<Icon path={mdiGithub} aria-label='github' size={1.5} className='inline-block text-slate-400 dark:text-slate-500' />
-		</a>
+			<a href='https://github.com/reeseovine/webtoys' className='mb-20'>
+				<Icon path={mdiGithub} aria-label='github' size={1.5} className='inline-block text-slate-400 dark:text-slate-500' />
+			</a>
+		</div>
 	</Page>
 )
 
