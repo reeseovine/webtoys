e8d8654da383828f79c59bc92824dc94793e8e89
Reese Sapphire <reese@ovine.xyz>
2022-03-21 21:31:08 -0500

    added custom error pages

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import Page from '@/components/page'
+
+const Error404 = () => {
+	return (
+		<Page title='404'>
+			<p>That page doesn&#x27;t exist.</p>
+		</Page>
+	)
+}
+
+export default Error404
---
+++ pages/500.tsx
@@ -0,0 +1,11 @@
+import Page from '@/components/page'
+
+const Error500 = () => {
+	return (
+		<Page title='500'>
+			<p>An error occurred on the server.</p>
+		</Page>
+	)
+}
+
+export default Error500
